499b8c057db1f7616193d5b94769a4215e88131d
jsbourdon <bourdon.js@gmail.com>
2020-04-22 12:01:44 -0400

    Adding missing UNREFERENCED_PARAMETER to wWinMain

--- LWGL_TestApp/LWGL_TestApp.cpp
+++ LWGL_TestApp/LWGL_TestApp.cpp
@@ -425,8 +425,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_ LPWSTR    lpCmdLine,
     _In_ int       nCmdShow)
 {
+    UNREFERENCED_PARAMETER(hInstance);
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
 
     RenderCore *pRenderCore = RenderCore::CreateCore();
     pRenderCore->Init<BaseRenderer>(L"Test", 1280, 720);
